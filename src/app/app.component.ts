9baafef84e3102b2bb95a3045712c1fe138a17c2
BalaVinci <balas@okit.in>
2021-11-01 12:34:53 +0530

    'init'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-moview';
+export class AppComponent implements OnInit {
+
+ 
+  ngOnInit():void{
+
+  }
+  
+  title = 'Custom';
+movies =[{
+  name:'Spider Man',
+  releaseDate:new Date()
+},
+{
+  name:'Spider Man',
+  releaseDate:new Date()
+}
+,
+{
+  name:'Spider Man',
+  releaseDate:new Date()
+},
+{
+  name:'Spider Man',
+  releaseDate:new Date()
+},
+{
+  name:'Spider Man',
+  releaseDate:new Date()
+}]
+
 }
